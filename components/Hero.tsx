b74741c56989da818cef3a7f85f7c9a2e0d46124
kilian <kilian.zimmerer@gmail.com>
2022-06-09 21:45:08 +0200

    wip: setup hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import hero from '../public/images/hero.png'
+import logo from '../public/images/logo_weiß_vertikal.png'
+import fancyObject from '../public/images/fancy_object.png'
+
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className='container-fluid hero p-0 m-0'>
+      <div className="row">
+        <div className="col">
+        </div>
+        <div className="col">
+          <img src={logo.src} className="img-fluid" height="100%" />
+        </div>
+        <div className="col">
+          <img src={fancyObject.src} className="img-fluid" height="100%" />
+        </div>
+      </div>
+    </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import Hero from '../components/Hero'
@@ -6,10 +8,14 @@ import Benefits from '../components/Benefits'
 import FAQ from '../components/FAQ'
 import Contact from '../components/Contact'
 
+import content from "../content/content.json"
+
 export default function Home() {
+  const [language, setLanguage] = useState("DE")
+
   return (
     <div>
-      <Header></Header>
+      <Header content={content[language]} setLanguage={setLanguage}></Header>
       <Hero></Hero>
       <Heading></Heading>
       <Benefits></Benefits>
