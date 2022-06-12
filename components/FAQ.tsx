6eda04d9530b5e3033a5872d95a02a7d6e20d3ae
kilian <kilian.zimmerer@gmail.com>
2022-06-12 20:09:15 +0200

    included faq bullets from content.json

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -1,24 +1,12 @@
 import React, { useState, FunctionComponent } from "react";
 import { TRUE } from "sass";
+import { StructuredType } from "typescript";
 
 export interface FAQData {
 
 }
 
 const FAQ: FunctionComponent<FAQData> = ({ header, FAQs }) => {
-  const [clickedButton1, setClickedButton1] = useState(false);
-  const [clickedButton2, setClickedButton2] = useState(false);
-
-
-  const buttonHandler = (event: any) => {
-    if (event.currentTarget.name === "button1") {
-      { clickedButton1 ? setClickedButton1(false) : setClickedButton1(true) }
-    }
-    else if (event.currentTarget.name === "button2") {
-      { clickedButton2 ? setClickedButton2(false) : setClickedButton2(true) }
-    }
-  };
-
   return (
     <div className='container-fluid pt-5'>
       <div className="row">
@@ -30,18 +18,9 @@ const FAQ: FunctionComponent<FAQData> = ({ header, FAQs }) => {
               {header}
             </p>
           </div>
-          <div className="row">
-            <button className="btn btn-primary-outline" name="button1" type="button" onClick={buttonHandler}>
-              <p className='text text-dark ps-3 float-start fs-5'> Wie kann ich mich beteiligen?</p> <p className='float-end text-primary fw-bold fs-1'>+</p>
-            </button>
-            <p className="text"> {clickedButton1 ? "This is the text" : ""} </p>
-          </div>
-          <div className="row">
-            <button className="btn" name="button2" type="button" onClick={buttonHandler}>
-              <p className='text text-dark ps-3 float-start fs-5'> blablabla?</p> <p className='float-end text-primary fw-bold fs-1'>+</p>
-            </button>
-            <p className="text"> {clickedButton2 ? `This is the text"` : ""} </p>
-          </div>
+          {FAQs.map(faq => (
+            <FAQBullet faq={faq} />
+          ))}
         </div>
         <div className="col-lg-1">
         </div>
@@ -50,21 +29,22 @@ const FAQ: FunctionComponent<FAQData> = ({ header, FAQs }) => {
   )
 }
 
-interface faqBulletData {
-
+interface FAQBulletData {
 }
 
-const faqBullet: FunctionComponent<faqBulletData> = () => {
+const FAQBullet: FunctionComponent<FAQBulletData> = ({faq}) => {
   const [clickedButton, setClickedButton] = useState(false);
-  const buttonHandler = (event: any) => { clickedButton ? setClickedButton(false) : setClickedButton(true) }
+  const buttonHandler = (event: any) => {
+    clickedButton ? setClickedButton(false) : setClickedButton(true)
+  }
   return (
     <div className="row">
-      <button className="btn btn-primary-outline" name="button1" type="button" onClick={buttonHandler}>
-        <p className='text text-dark ps-3 float-start fs-5'> Wie kann ich mich beteiligen?</p> <p className='float-end text-primary fw-bold fs-1'>+</p>
+      <button className="btn" name="button1" type="button" onClick={buttonHandler}>
+        <p className='text text-dark ps-3 float-start fs-5'> {faq["question"]} </p> <p className='float-end text-primary fw-bold fs-1'>+</p>
       </button>
-      <p className="text"> {clickedButton ? "This is the text" : ""} </p>
+      <p className="text"> {clickedButton ? faq["answer"] : ""} </p>
     </div>
-  );
-};
+  )
+}
 
 export default FAQ;
