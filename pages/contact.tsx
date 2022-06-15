55ec21dc1428da128f63df667c1d156cb4ccb850
KilianZimmerer <21954712+KilianZimmerer@users.noreply.github.com>
2022-06-15 11:27:41 +0200

    set up basic construc of contact and success page

---
+++ pages/contact.tsx
@@ -0,0 +1,35 @@
+import React, { useState, FunctionComponent } from "react";
+
+import Header, { HeaderData } from "../components/Header";
+import Hero from "../components/Hero";
+import Heading, { HeadingData } from "../components/Heading";
+import ContactForm, { ContactFormData } from "../components/ContactForm";
+import Footer, { FooterData } from "../components/Footer";
+
+import content from "../content/content.json";
+
+
+interface LocalizedContent {
+    Header: HeaderData;
+    Heading: HeadingData;
+    contactForm: ContactFormData;
+    Footer: FooterData;
+}
+
+export type Language = "EN" | "DE";
+
+const Contact: FunctionComponent = () => {
+    const [language, setLanguage] = useState<Language>("DE");
+    const localizedContent: LocalizedContent = content[language];
+    return (
+        <div>
+            <Header {...localizedContent["Header"]} setLanguage={setLanguage} />
+            <Hero />
+            <Heading {...localizedContent["Heading"]}></Heading>
+            <ContactForm {...localizedContent["contactForm"]} language={language} />
+            <Footer {...localizedContent["Footer"]} />
+        </div>
+    );
+};
+
+export default Contact;
--- pages/success.tsx
+++ pages/success.tsx
@@ -1,9 +1,23 @@
-import React, { FunctionComponent } from "react";
+import React, { useState, FunctionComponent } from "react";
 import { useRouter } from "next/router";
+
+
+import Header, { HeaderData } from "../components/Header";
+import Hero from "../components/Hero";
+import Heading, { HeadingData } from "../components/Heading";
+import Footer, { FooterData } from "../components/Footer";
+
 import content from "../content/content.json";
-import { Language } from "./index";
 
 type SuccessPageContent = Record<string, string>;
+
+interface LocalizedContent {
+  Header: HeaderData;
+  Heading: HeadingData;
+  Footer: FooterData;
+}
+
+export type Language = "EN" | "DE";
 
 const Success: FunctionComponent = () => {
   const router = useRouter();
@@ -14,10 +28,17 @@ const Success: FunctionComponent = () => {
   }
   const successpageContent: SuccessPageContent =
     content[lang]["contactForm"]["successPage"];
+  const [language, setLanguage] = useState<Language>("DE");
+  const localizedContent: LocalizedContent = content[language];
   return (
-    <div className="container-fluid p-5">
-      <h4 className="text-center text-dark">{successpageContent["header"]}</h4>
-      <p className="text-center text-dark">{successpageContent["text"]}</p>
+    <div>
+      <Header {...localizedContent["Header"]} setLanguage={setLanguage} />
+      <Hero />
+      <div className="container-fluid p-5">
+        <h4 className="text-center text-dark">{successpageContent["header"]}</h4>
+        <p className="text-center text-dark">{successpageContent["text"]}</p>
+      </div>
+      <Footer {...localizedContent["Footer"]} />
     </div>
   );
 };
