93c26ecd5a7e08c78f62dea23f0533e86205654a
KilianZimmerer <21954712+KilianZimmerer@users.noreply.github.com>
2022-06-20 19:22:30 +0200

    add logoAction event to index page onClick

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, FunctionComponent } from "react";
+import { useRouter } from "next/router";
 
 import Header, { HeaderData } from "../components/Header";
 import FAQ, { FAQData } from "../components/FAQ";
@@ -6,7 +7,7 @@ import Footer, { FooterData } from "../components/Footer";
 import Hero from "../components/Hero";
 import Heading, { HeadingData } from "../components/Heading";
 import Benefits, { BenefitsData } from "../components/Benefits";
-import Contact, {ContactData} from "../components/Contact";
+import Contact, { ContactData } from "../components/Contact";
 
 import content from "../content/content.json";
 
@@ -22,20 +23,18 @@ interface LocalizedContent {
 export type Language = "EN" | "DE";
 
 const Home: FunctionComponent = () => {
-  const [language, setLanguage] = useState<Language>("DE");
+  const router = useRouter();
+  const lang = (router.query.state && router.isReady) ? router.query.state as Language : "DE"
+  const [language, setLanguage] = useState<Language>(lang);
   const localizedContent: LocalizedContent = content[language];
-
   return (
     <div>
-      <Header
-        {...localizedContent["Header"]}
-        setLanguage={setLanguage}
-      ></Header>
-      <Hero></Hero>
+      <Header {...localizedContent["Header"]} setLanguage={setLanguage} />
+      <Hero language={language} />
       <div className={"container-sm"}>
         <Heading {...localizedContent["Heading"]}></Heading>
         <Benefits {...localizedContent["Benefits"]}></Benefits>
-        <Contact {...localizedContent["Contact"]} language={language}/>
+        <Contact {...localizedContent["Contact"]} language={language} />
         <FAQ {...localizedContent["FAQ"]}></FAQ>
       </div>
       <Footer {...localizedContent["Footer"]} language={language} />
