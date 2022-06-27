950137be5f3a0dc26dd3fc796fbcbcebf2f1e6a3
jakobkolb <jakob.j.kolb@gmail.com>
2022-06-27 10:56:02 +0200

    Fix user component layout.

--- components/Login.tsx
+++ components/Login.tsx
@@ -2,63 +2,90 @@ import { signIn, signOut, useSession } from "next-auth/react";
 import Image from "next/image";
 import { FunctionComponent } from "react";
 
+type User = {
+  name: string
+  email: string
+  image: string
+}
+
 const Login: FunctionComponent = () => {
   const { data: session, status } = useSession();
   const loading = status === "loading";
-  console.log(session?.user);
+  const user = session?.user as User
+  console.log(user);
   return (
-    <div>
+    <div className={'container'}>
       {!loading && (
         <>
-          {!session && (
-            <a
-              href={`/api/auth/signin`}
-              className={"btn btn-primary my-2 my-sm-0"}
-              onClick={(e) => {
-                e.preventDefault();
-                signIn();
-              }}
-            >
-              Sign in
-            </a>
-          )}
-          {session?.user && (
-            <div className={"row align-items-center justify-content-end"}>
-              {/*{session.user.image && (*/}
-              {/*  <div className={"col-2"}>*/}
-              {/*    <Image*/}
-              {/*      className={"rounded-circle"}*/}
-              {/*      src={session.user.image}*/}
-              {/*      width={32}*/}
-              {/*      height={32}*/}
-              {/*    />*/}
-              {/*  </div>*/}
-              {/*)}*/}
-
-              <div className={"col-5"}>
-                <span className={"text-white fw-bold text-nowrap"}>
-                  {session.user.name ?? session.user.email}
-                </span>
-              </div>
-
-              <div className={"col-5 pe-4"}>
-                <a
-                  href={`/api/auth/signout`}
-                  className={"btn btn-light text-nowrap"}
-                  onClick={(e) => {
-                    e.preventDefault();
-                    signOut();
-                  }}
-                >
-                  Sign out
-                </a>
-              </div>
-            </div>
-          )}
+          {!session &&
+              <SignInButton signIn={signIn}/>
+          }
+          {session?.user &&
+            <SignedInUser signOut={signOut} user={user}/>
+          }
         </>
       )}
     </div>
   );
 };
+
+interface SignInProps {
+  signIn: () => Promise<void>
+}
+
+const SignInButton: FunctionComponent<SignInProps> = ({signIn}) => {
+  return (<a
+      href={`/api/auth/signin`}
+      className={"btn btn-primary my-2 my-sm-0"}
+      onClick={(e) => {
+        e.preventDefault();
+        signIn();
+      }}
+  >
+    Sign in
+  </a>)
+}
+
+interface SignOutProps {
+  signOut: () => Promise<void>
+  user: User
+}
+
+const SignedInUser: FunctionComponent<SignOutProps> = ({signOut, user}) => {
+  return (
+    <div className={"row align-items-center"}>
+      {user.image && (
+        <div className={"col-2 px-1  d-flex justify-content-end"}>
+          <Image
+            className={"rounded-circle"}
+            src={user.image}
+            layout={'fixed'}
+            width={37}
+            height={37}
+          />
+        </div>
+      )}
+
+      <div className={"col-5 d-flex justify-content-center"}>
+                    <span className={"text-white fw-bold text-nowrap"}>
+                      {user.name ?? user.email}
+                    </span>
+      </div>
+
+      <div className={"col-5 d-flex justify-content-start"}>
+        <a
+            href={`/api/auth/signout`}
+            className={"btn btn-light text-nowrap"}
+            onClick={(e) => {
+              e.preventDefault();
+              signOut();
+            }}
+        >
+          Sign out
+        </a>
+      </div>
+    </div>
+  )
+}
 
 export default Login;
